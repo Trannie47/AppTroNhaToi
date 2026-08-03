95b374d8573dc921ac86bc5d37b55b61088beb38
Trannie47 <tranghuyen2012003@gmail.com>
2026-08-04 02:00:33 +0700

    fix: update commented base URL in HttpConfig for clarity

--- lib/core/constants/http.dart
+++ lib/core/constants/http.dart
@@ -1,6 +1,6 @@
 class HttpConfig {
   static const String baseUrl = 'http://10.0.2.2:3000/';
+  //quanlynhatroapi.thientammedical.io.vn/
   // Image
   static const String imageBaseUrl = '$baseUrl/uploads/';
-
 }
